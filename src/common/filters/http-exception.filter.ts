13dc27e25c1d1e2119d1e1212fe591e0744b891a
Biwash Pradhan <biwashpra3883@gmail.com>
2026-07-24 22:08:54 +0545

    feat: use of global http exception filters and initialize swagger

---
+++ src/common/filters/http-exception.filter.ts
@@ -0,0 +1,43 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import type { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+
+    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<Response>();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    let message: unknown = 'Internal Server Error';
+
+    if (exception instanceof HttpException) {
+      const exceptionResponse = exception.getResponse();
+
+      if (typeof exceptionResponse === 'string') {
+        message = exceptionResponse;
+      } else {
+        const res = exceptionResponse as Record<string, unknown>;
+        message = res.message ?? 'Internal Server Error';
+      }
+    }
+
+    response.status(status).json({
+      statusCode: status,
+      message,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
+
+  const configService = app.get(ConfigService);
+
+  app.use(cookieParser());
+
+  app.setGlobalPrefix('api');
+
+  // This is useful while using DTO
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('NestJS Auth Course')
+    .setDescription('The complete authentication system.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = configService.get<number>('PORT') ?? 5000;
+
+  await app.listen(port);
+
+  console.log(`Server running on port http://localhost:${port}/api`);
+  console.log(`Swagger docs at http://localhost:${port}/api/docs`);
 }
 bootstrap();
