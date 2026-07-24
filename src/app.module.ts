0ddc6253229b4c9fb6e228e1e6885ca10003ef9c
Biwash Pradhan <biwashpra3883@gmail.com>
2026-07-24 18:12:50 +0545

    feat: configure the env config at root level

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true, // This means we can assign one env value to another key APP_URL=${BASE_URL
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
